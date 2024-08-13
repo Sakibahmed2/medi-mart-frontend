d37166bfe7ec598672e8d8a0b8a40da0134cb47e
unknown <mauijaloskor@gmail.com>
2024-08-13 10:41:39 +0600

    init

---
+++ src/lib/providers/Providers.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React, { ReactNode } from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "../theme/theme";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
+
+const Providers = ({ children }: { children: ReactNode }) => {
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </Provider>
+  );
+};
+
+export default Providers;
